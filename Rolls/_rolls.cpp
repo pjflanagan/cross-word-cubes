54af1891d93a0a501f92e682f3bee2898fe4675e
Peter James Flanagan <pjflanagan1@gmail.com>
2018-01-01 16:12:55 -0500

    working scores

--- Rolls/_rolls.cpp
+++ Rolls/_rolls.cpp
@@ -2,25 +2,31 @@
 #include <iostream>
 #include <deque>
 #include <sstream>
-#include <utilitiy>
+#include <fstream>
 
 
-#include "Scores.cpp"
+#include "../Scores.cpp"
 #include "Cube.cpp"
 
 
 int main(int argc, char *argv[]){
 
-	ios_base::sync_with_stdio(false);
-
-	ostringstream os; 
+	std::ios_base::sync_with_stdio(false);
+	
+	std::ostringstream os; //os << scores['z'] << "\n";
+	
+	//initialize the scorecard
+	Scores scores = Scores();
 
 	//initialize cubes listed in the cubes.txt
 	std::vector<Cube> cubes;
 
-	//make the first million of the 6^14 options for rolls 
+	//make the first 100 thousand of the 6^14 options for rolls 
 	//and save in a text file to be used later
 	//use a priority queue of rolls and once you get to
 	//like 10 million save the first million
 
+	//print out the rolls
+	std::cout << os.str() << std::flush;
+	
 }
--- Scores.cpp
+++ Scores.cpp
@@ -1,16 +1,36 @@
+#ifndef SCORES_CPP
+#define SCORES_CPP
+
 #include <map>
+#include <fstream>
 
 class Scores {
 	public:
-	Scores(){
-		//reads the scores.txt file and makes the scores map
-	}
-
-	int operator[](const char c) const {
-		return scores[c];
-	}
+	Scores();
+	int operator[](const char c) const;
 
 	private:
-	static std::map<char, int> scores;
-
+	int value(const char c) const;
+	std::map<char, int> scores;
 }; 
+
+Scores::Scores(){
+	//reads the scores.txt file and makes the scores map
+	std::ifstream score_file("scores.txt");
+	scores = std::map<char, int>();
+	char character; 
+	int value;
+	while(score_file >> character >> value){
+		Scores::scores.insert(std::pair<char,int>(character, value));
+	}
+}
+
+int Scores::operator[](const char c) const {
+	return value(c);
+}
+
+int Scores::value(const char c) const {
+	return scores.at(c);
+}
+
+#endif
